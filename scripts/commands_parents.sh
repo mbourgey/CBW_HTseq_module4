46d46049b2c4b08ce696d69afe1be345baf03bd9
Mathieu Bourgey <mathieu.bourgey@mcgill.ca>
2021-09-17 11:24:54 -0400

    update to HTG 2021

--- scripts/commands_parents.sh
+++ scripts/commands_parents.sh
@@ -137,3 +137,46 @@ java -Xmx2g -jar $GATK_JAR -T VariantAnnotator \
 module unload mugqic/GenomeAnalysisTK/3.8
 module load  mugqic/GenomeAnalysisTK/4.1.0.0
 
+
+### Trio mode
+java -Xmx2g -jar $GATK_JAR HaplotypeCaller \
+-R $REF/genome/Homo_sapiens.GRCh37.fa \
+-I bam/NA12878/NA12878.sorted.dup.recal.bam \
+-I bam/NA12891/NA12891.sorted.dup.recal.bam \
+-I bam/NA12892/NA12892.sorted.dup.recal.bam \
+-O variants/trio.rmdup.realign.hc.vcf \
+-L 1:17704860-18004860
+
+
+
+java -Xmx2g -jar $GATK_JAR VariantFiltration \
+-R $REF/genome/Homo_sapiens.GRCh37.fa \
+-V variants/trio.rmdup.realign.hc.vcf \
+-O variants/trio.rmdup.realign.hc.filter.vcf \
+-filter "QD < 2.0" \
+-filter "FS > 200.0" \
+-filter "MQ < 40.0" \
+--filter-name QDFilter \
+--filter-name FSFilter \
+--filter-name MQFilter
+
+java -Xmx4G -jar $SNPEFF_HOME/snpEff.jar eff \
+-v -no-intergenic \
+-i vcf -o vcf GRCh37.75 variants/trio.rmdup.realign.hc.filter.vcf >  variants/trio.rmdup.realign.hc.filter.snpeff.vcf
+
+
+#switch to old GATK 3.8
+module unload  mugqic/GenomeAnalysisTK/4.1.0.0
+module load mugqic/GenomeAnalysisTK/3.8
+
+java -Xmx2g -jar $GATK_JAR -T VariantAnnotator \
+-R $REF/genome/Homo_sapiens.GRCh37.fa \
+--dbsnp $REF/annotations/Homo_sapiens.GRCh37.dbSNP150.vcf.gz \
+-V variants/trio.rmdup.realign.hc.filter.snpeff.vcf \
+-o variants/trio.rmdup.realign.hc.filter.snpeff.dbsnp.vcf \
+-L 1:17704860-18004860
+
+#return to GATK 4
+module unload mugqic/GenomeAnalysisTK/3.8
+module load  mugqic/GenomeAnalysisTK/4.1.0.0
+
--- solutions/_trio4.md
+++ solutions/_trio4.md
@@ -1,1 +1,8 @@
-Most of the variant have keep the same genotpes but it improve call in supicious site such as 1:17,841,784-17,841,823. In a general manner the more information is provided, the more accurate the genotype call are.   
+Most of the variant have keep the same genotpes but it improve call in supicious site such as 1:17,841,784-17,841,823.
+
+Also some new variant appears such as the one at position 1:17,826,542-17,826,580. This one has been called but then we removed it due to low QD.
+
+Or at position 1:17,822,491-17,822,569 where the variant was missed at individual call.
+
+
+In a general manner the more information is provided, the more accurate and the more sensitive the genotype call are.   
